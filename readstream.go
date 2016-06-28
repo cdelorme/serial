aef5f2843600d3ca77984b02dc12127df3ecc3f2
Casey DeLorme <cdelorme@gmail.com>
2016-06-28 10:56:28 -0400

    add explicit types and code re-use for read and write logic

--- readstream.go
+++ readstream.go
@@ -24,13 +24,56 @@ func (self *ReadStream) SerializeString(in *string) error {
 	return nil
 }
 
-func (self *ReadStream) SerializeInt(in *int) error {
+func (self *ReadStream) SerializeInt(in *int) (e error) {
 	var l int64
-	if e := binary.Read(self, ByteOrder, &l); e != nil {
-		return e
-	}
-
+	e = self.SerializeInt64(&l)
 	*in = int(l)
+	return
+}
 
-	return nil
+func (self *ReadStream) SerializeUint(in *uint) (e error) {
+	var l uint64
+	e = self.SerializeUint64(&l)
+	*in = uint(l)
+	return
+}
+
+func (self *ReadStream) SerializeInt8(in *int8) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeInt16(in *int16) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeInt32(in *int32) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeInt64(in *int64) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeUint8(in *uint8) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeUint16(in *uint16) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeUint32(in *uint32) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeUint64(in *uint64) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeFloat32(in *float32) error {
+	return binary.Read(self, ByteOrder, in)
+}
+
+func (self *ReadStream) SerializeFloat64(in *float64) error {
+	return binary.Read(self, ByteOrder, in)
 }
--- writestream.go
+++ writestream.go
@@ -17,5 +17,51 @@ func (self *WriteStream) SerializeString(out *string) error {
 }
 
 func (self *WriteStream) SerializeInt(out *int) error {
-	return binary.Write(self, ByteOrder, int64(*out))
+	l := int64(*out)
+	return self.SerializeInt64(&l)
+}
+
+func (self *WriteStream) SerializeUint(out *uint) error {
+	l := uint64(*out)
+	return self.SerializeUint64(&l)
+}
+
+func (self *WriteStream) SerializeInt8(out *int8) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeInt16(out *int16) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeInt32(out *int32) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeInt64(out *int64) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeUint8(out *uint8) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeUint16(out *uint16) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeUint32(out *uint32) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeUint64(out *uint64) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeFloat32(out *float32) error {
+	return binary.Write(self, ByteOrder, *out)
+}
+
+func (self *WriteStream) SerializeFloat64(out *float64) error {
+	return binary.Write(self, ByteOrder, *out)
 }
